6bd9b8c29f6de6e8778c53e65b133eae358b9805
kecy <keishrio.oyama@linecorp.com>
2020-03-17 03:08:13 +0900

    add storybook docs dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.([tj]sx|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-docs',
   ],
 };
