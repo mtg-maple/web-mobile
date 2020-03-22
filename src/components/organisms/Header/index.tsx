03650152a6511a38b10eb9a518701de7d60c0851
kecy <keishrio.oyama@linecorp.com>
2020-03-22 17:29:41 +0900

    add organisms/Header

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { Heading, Select, SelectProps, isSelectProps, isIconButtonProps, IconButton, IconButtonProps } from '@mtg-maple/web-components';
+
+import styles from './style.module.scss';
+
+export type HeaderProps = {
+  headingText: string;
+  funcProps?: SelectProps | IconButtonProps;
+};
+
+const Header: FC<HeaderProps> = ({ headingText, funcProps }) => {
+  return (
+    <div className={styles.header}>
+      <Heading level='1'>{headingText}</Heading>
+      {
+        funcProps &&
+        (
+          isSelectProps(funcProps) ? <Select {...funcProps}/> :
+          isIconButtonProps(funcProps) ? <IconButton {...funcProps}/> :
+          ''
+        )
+      }
+    </div>
+  );
+};
+
+export default Header;
