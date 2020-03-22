33de9681c84c7a05c74be4115e1067727f9bfb69
kecy <keishrio.oyama@linecorp.com>
2020-03-23 07:50:01 +0900

    add organisms/NavigationBar

---
+++ src/components/organisms/NavigationBar/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  NavLink
+} from 'react-router-dom';
+
+import styles from './style.module.scss';
+
+export type NavigationBarProps = {
+  links: NavigationLink[];
+  className?: string;
+}
+
+export type NavigationLink = {
+  icon: IconDefinition;
+  link: string;
+}
+
+const NavigationBar: FC<NavigationBarProps> = ({ links, className }) => {
+  return (
+    <ul className={[styles.navigationBar, className].join(' ')}>
+      {
+        links.map((props: NavigationLink) => (
+          <li className={styles.navigationBarItem}>
+            <NavLink to={props.link} className={styles.navigationBarLink} activeStyle={{ color: 'red' }}>
+              <FontAwesomeIcon icon={props.icon}/>
+            </NavLink>
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+export default NavigationBar;
