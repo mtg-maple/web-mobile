94b955bb459c21e9e8d6dc9a9f52a8972895fb96
kecy <keishrio.oyama@linecorp.com>
2020-03-23 09:43:56 +0900

    change structure of SearchBar props

--- src/components/organisms/SearchBar/index.tsx
+++ src/components/organisms/SearchBar/index.tsx
@@ -5,35 +5,34 @@ import { IconButton, Input, Tags, Tag } from '@mtg-maple/web-components';
 import styles from './style.module.scss';
 
 export type SearchBarProps = {
-  query: string;
-  setQuery: (newQuery: string) => void;
+  queryState: [string, (newQuery: string) => void];
+  tagsState: [Tag[], (newTags: any[]) => void];
   placeholder?: string;
-  tags: Tag[];
-  setTags: (newTags: any[]) => void;
   onClick: (e: React.MouseEvent) => void;
+  className?: string;
 };
 
-const SearchBar: FC<SearchBarProps> = ({ query, setQuery, placeholder = '', tags, setTags, onClick }) => {
+const SearchBar: FC<SearchBarProps> = ({ queryState, placeholder = '', tagsState, onClick, className }) => {
   return (
-    <div className={styles.searchBarWrap}>
+    <div className={[styles.searchBarWrap, className].join(' ')}>
       <div className={styles.searchBar}>
         <Input
-          value={query}
-          setValue={setQuery}
+          value={queryState[0]}
+          setValue={queryState[1]}
           placeholder={placeholder}
           icon={faSearch}
         />
         <IconButton 
           className={styles.settingButton}
           icon={faSlidersH}
-          color={tags.length > 0 ? 'primary' : 'muteText'}
+          color={tagsState[0].length > 0 ? 'primary' : 'muteText'}
           onClick={onClick}
         />
       </div>
       {
-        tags.length > 0 &&
+        tagsState[0].length > 0 &&
         <div className={styles.tags}>
-          <Tags tags={tags} setTags={setTags}/>
+          <Tags tags={tagsState[0]} setTags={tagsState[1]}/>
         </div>
       }
     </div>
