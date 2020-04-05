c59433ca91c1ef8cd5642627461b3309c1620f58
kecy <keishrio.oyama@linecorp.com>
2020-04-05 14:03:40 +0900

    add DeckSummary to DeckTemplate

--- src/components/templates/DeckTemplate/index.tsx
+++ src/components/templates/DeckTemplate/index.tsx
@@ -1,8 +1,10 @@
-import React, { FC, MouseEvent } from 'react';
+import React, { FC, MouseEvent, useState, useEffect, useRef } from 'react';
 import * as H from 'history';
 
 import NavigationHeader from '../../organisms/NavigationHeader'
+import DeckSummary from '../../organisms/DeckSummary';
 import { IDeck } from '../../../store';
+import styles from './style.module.scss';
 
 export type DeckTemplateProps = {
   deck: IDeck;
@@ -12,9 +14,35 @@ export type DeckTemplateProps = {
   };
 }
 
-const DeckTemplate: FC<DeckTemplateProps> = ({ deck, backButtonProps }) => (
-    <>
-      <NavigationHeader title={deck.name} backButtonProps={backButtonProps}/>
+const DeckTemplate: FC<DeckTemplateProps> = ({ deck, backButtonProps }) => {
+  const [belowTopSection, setBelowTopSection] = useState(false);
+  const anchorRef = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (anchorRef !== null && anchorRef.current !== null) {
+        console.log(anchorRef.current.getBoundingClientRect().bottom);
+        if (anchorRef.current.getBoundingClientRect().bottom > 0) {
+          setBelowTopSection(false);
+        } else {
+          setBelowTopSection(true);
+        }
+      }
+    }
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+  return (
+    <div className={styles.pageRoot}>
+      <NavigationHeader
+        className={styles.navigationHeader}
+        title={deck.name}
+        backButtonProps={backButtonProps}
+        isSimple={belowTopSection}
+      />
+      <DeckSummary deck={deck} />
+      <span style={{ visibility: 'hidden' }} ref={anchorRef}></span>
       <p>lorem ipsum</p>
       <p>lorem ipsum</p>
       <p>lorem ipsum</p>
@@ -50,8 +78,8 @@ const DeckTemplate: FC<DeckTemplateProps> = ({ deck, backButtonProps }) => (
       <p>lorem ipsum</p>
       <p>lorem ipsum</p>
       <p>lorem ipsum</p>
-      <p>lorem ipsum</p>
-    </>
-);
+    </div>
+  )
+};
 
 export default DeckTemplate;
